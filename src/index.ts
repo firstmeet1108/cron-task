cf16555b02fa07aa1480af3ed4df961ddae42c22
firstmeet <2022742378@qq.com>
2024-05-07 00:14:54 +0800

    feat: epic案例完成

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Context, Schema } from 'koishi';
+import {} from 'koishi-plugin-cron';
 import { commandGeneratorMap, cronTaskGeneratorMap } from './ganeraterMap';
 import { xml2js } from 'xml-js';
+import dayjs from 'dayjs';
 
 export const name = 'rss';
 declare module 'koishi' {
@@ -12,25 +14,30 @@ declare module 'koishi' {
 interface CronTask {
   id: number;
   name: string;
-  target_id: number;
+  target_id: string;
+  type: 'private' | 'group';
   create_time: Date;
 }
 
 export interface TaskHandlerMap {
-  epic: (userContext: UserContext, ctx: Context) => any;
+  epic: (ctx: Context) => any;
   // steam: () => void;
   // github: () => void;
 }
 
 const taskHandlerMap: TaskHandlerMap = {
-  epic: async ({}, ctx) => {
+  epic: async (ctx) => {
     const toRenderData = await getRssData(
       'https://rsshub.app/epicgames/freegames/zh-CN',
       ctx
     );
+    // console.log(toRenderData);
+    // const id = toRenderData.lastBuildDate._text;
+    // const date = dayjs(id);
+    // console.log(date);
     const toSendData = cronTaskGeneratorMap['epic'](toRenderData);
 
-    return toRenderData;
+    return toSendData;
   },
   // steam: () => {
   //   console.log('STEAM');
@@ -41,8 +48,9 @@ const taskHandlerMap: TaskHandlerMap = {
 };
 
 type UserContext = {
-  target_id: number;
+  target_id: string;
   task_name: keyof TaskHandlerMap;
+  type: 'private' | 'group';
   option: keyof CommandOptionsHandlerMap;
 };
 
@@ -59,11 +67,12 @@ interface CommandOptionsHandlerMap {
 }
 
 const commandOptionsHandlerMap: CommandOptionsHandlerMap = {
-  subscribe: async ({ target_id, task_name }, ctx) => {
+  subscribe: async ({ target_id, task_name, type }, ctx) => {
     // 订阅
     const res = await ctx.database.create('cron_task', {
       name: task_name,
       target_id,
+      type: type,
       create_time: new Date(),
     });
     return {
@@ -90,6 +99,8 @@ const commandOptionsHandlerMap: CommandOptionsHandlerMap = {
   },
 };
 
+export const inject = ['cron'];
+
 export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({});
@@ -101,12 +112,13 @@ export async function apply(ctx: Context) {
     {
       id: 'unsigned',
       name: 'string',
-      target_id: 'integer',
+      target_id: 'string',
+      type: 'string',
       create_time: 'timestamp',
     },
     {
       autoInc: true,
-      unique: [['name', 'target_id']],
+      unique: [['name', 'target_id', 'type']],
     }
   );
   // 命令模块
@@ -160,7 +172,8 @@ export async function apply(ctx: Context) {
 
       // 当前可用数据
       const userContext: UserContext = {
-        target_id, // 群号或者用户号
+        target_id: target_id.toString(), // 群号或者用户号
+        type: messageData.message_type,
         task_name: task_name as keyof TaskHandlerMap, // 任务名
         option, // 选项
       };
@@ -177,27 +190,53 @@ export async function apply(ctx: Context) {
       authority: 2,
     })
     .action(async ({ session }) => {
-      console.log('test');
       const onebot = ctx.bots[0];
-      await onebot.sendPrivateMessage('2022742378', 'test');
+      // taskHandlerMap['epic']({}, ctx);
+
+      // await onebot.sendPrivateMessage('2022742378', 'test');
       // for (const task_name in taskHandlerMap) {
-      //   console.log(task_name);
-      //   const target_id_arr = await ctx.database.get(
+      //   const dataList = await ctx.database.get(
       //     'cron_task',
       //     {
       //       name: task_name,
       //     },
-      //     ['target_id']
+      //     ['target_id', 'type']
       //   );
-      //   target_id_arr
-      //     .map((e) => e.target_id.toString())
-      //     .forEach(async (e) => {
-      //       console.log(e);
-      //       await onebot.sendMessage(e, 'epic');
-      //     });
+      //   console.log(dataList);
+      //   dataList.forEach(async (e) => {
+      //     if (e.type === 'group') {
+      //       await onebot.sendMessage(e.target_id, 'epic');
+      //     } else {
+      //       await onebot.sendPrivateMessage(e.target_id, 'epic');
+      //     }
+      //   });
       // }
     });
+
   // 定时任务模块
+  // 0 0 12 ? * FRI 每周五中午12点
+  // 0/5 * * * * ? 每5秒
+  ctx.cron('0 0 12 ? * FRI', async () => {
+    const onebot = ctx.bots[0];
+    const toSendData = await taskHandlerMap['epic'](ctx);
+    for (const task_name in taskHandlerMap) {
+      const dataList = await ctx.database.get(
+        'cron_task',
+        {
+          name: task_name,
+        },
+        ['target_id', 'type']
+      );
+      console.log(dataList);
+      dataList.forEach(async (e) => {
+        if (e.type === 'group') {
+          await onebot.sendMessage(e.target_id, toSendData);
+        } else {
+          await onebot.sendPrivateMessage(e.target_id, toSendData);
+        }
+      });
+    }
+  });
 }
 // 任务动作
 // toRenderData = await taskHandlerMap[task_name](userContext, ctx);
